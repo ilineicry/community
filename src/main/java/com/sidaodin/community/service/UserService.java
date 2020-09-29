f790b79d02eaf48173151ee5cc8f02a2fdbc33f5
ilineicry <leileijava@126.com>
2020-09-29 15:48:07 +0800

    首次提交

---
+++ src/main/java/com/sidaodin/community/service/UserService.java
@@ -0,0 +1,21 @@
+package com.sidaodin.community.service;
+
+import com.sidaodin.community.dao.UserMapper;
+import com.sidaodin.community.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+
+    @Autowired
+    private UserMapper userMapper;
+
+    public User findUserById(int id) {
+        return userMapper.selectById(id);
+    }
+
+}
+
+
